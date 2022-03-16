231d0ea2adb06f4d985d22fc79ef3e0d1707a6b6
AjinkyaChandekar <65499227+AjinkyaChandekar@users.noreply.github.com>
2022-03-16 19:33:38 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -4,13 +4,14 @@ terraform {
     bucket = "app01-prod01"
     key    = "app01-prod01.terraform.tfstate"
     region = "us-east-1"
-    dynamodb_table = "s3_remote_state"
-    access_key = "AKIA3OTBACMSQGJ5G64F"
-    secret_key = "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"
+    #dynamodb_table = "s3_remote_state"
+    #access_key = "AKIA3OTBACMSQGJ5G64F"
+    #secret_key = "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"
+    access_key = ${{ secrets.access_key }}
+    secret_key = ${{ secrets.secret_key }}
   }
 }
 */
-
 # Using a single workspace from Terraform Cloud:
 terraform {
   backend "remote" {
@@ -18,7 +19,7 @@ terraform {
     organization = "dmart"
 
     workspaces {
-      name = "Prod-Digital-Mindtree"
+      name = "Terraform-docker"
     }
   }
 }
